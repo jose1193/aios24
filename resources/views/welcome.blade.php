abee1c732a943977594ab168a17ca3edf2a971d3
jose1193 <josegonzalezcr2794@gmail.com>
2023-04-27 01:36:44 +0100

    fix: welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#22c55e" />
 
+    <link rel="icon" href="{{ asset('img/favicon.ico') }}">
     <title>AIOS Real Estate </title>
 
     <!-- Fonts -->
