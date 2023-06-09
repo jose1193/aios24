fa5b6032f13793c5bf1905102177f8ceb27959be
jose1193 <josegonzalezcr2794@gmail.com>
2023-06-10 00:26:26 +0100

    Add: Button Show Post on post crud

--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -31,7 +31,7 @@
                              <th class="px-4 py-2">Image</th>
                              <th class="px-4 py-2">Title</th>
                              <th class="px-4 py-2">Content</th>
-                             <th class="px-4 py-2">Category</th>
+                             <th class="px-4 py-2"></th>
                              <th class="px-4 py-2">Status</th>
                              <th class="px-4 py-2">Action</th>
                          </thead>
@@ -39,7 +39,7 @@
                              <tr></tr>
                              @forelse($posts as $post)
                                  <tr>
-                                     <td class="px-6 py-4 whitespace-nowrap">{{ $post->id }}</td>
+                                     <td class="px-6 py-4 whitespace-nowrap">{{ $loop->iteration }}</td>
 
                                      <td class="px-6 py-4 whitespace-nowrap ">
                                          <div class="w-1/2 mx-auto">
@@ -51,12 +51,21 @@
                                      <td class="px-6 py-4 whitespace-nowrap">
                                          {{ Str::words($post->post_title, 6, '...') }}</td>
                                      <td class="px-6 py-4 whitespace-nowrap">
-                                         {{ Str::words($post->post_content, 7, '...') }} </td>
+                                         {{ Str::words($post->post_content, 4, '...') }} </td>
                                      <td class="px-6 py-4 whitespace-nowrap">{{ $post->name }}</td>
                                      <td class="px-6 py-4 whitespace-nowrap">{{ $post->post_status }}</td>
 
                                      <td class="px-6 py-4 text-center text-sm">
+                                         <a href="{{ route('showpost', ['postTitle' => $post->post_title_slug]) }}"
+                                             class="bg-indigo-700 transition duration-500 ease-in-out hover:bg-indigo-600 text-white font-bold  inline-flex items-center p-2 px-4 py-2 mr-0.5 rounded">
+                                             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5  inline-block "
+                                                 viewBox="0 0 576 512" stroke="currentColor">
+                                                 <path
+                                                     d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"
+                                                     fill="#ffffff" />
+                                             </svg>
 
+                                         </a>
                                          <x-button wire:click="showEditDataModal({{ $post->id }})">
                                              <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 inline-block"
                                                  fill="none" viewBox="0 0 24 24" stroke="currentColor">
